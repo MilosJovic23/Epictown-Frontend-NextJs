896558913f659bbb3fcf6ccb052d02e98ea2e347
MilosJovic23 <milos.mj992@gmail.com>
2024-09-10 09:02:03 +0200

    * initial commit

---
+++ src/app/Products/[slug]/page.js
@@ -0,0 +1,26 @@
+
+
+
+export default function Products (){
+
+
+
+
+    // if( !data ){
+    //     return <h1>Ova stranice ne postoji</h1>
+    // }
+
+    return<>
+
+        <div>
+            <h3>test</h3>
+            <p>test2</p>
+            <p>test3</p>
+            <b>test2</b>
+            <p>test</p>
+        </div>
+
+    </>
+
+
+};
