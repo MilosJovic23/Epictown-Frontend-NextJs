14a1e27526148221b136f9c1a00b4cf6dd1fe716
MilosJovic23 <milos.mj992@gmail.com>
2024-09-11 11:29:42 +0200

    * Products slug page setup

--- src/app/Products/[slug]/page.js
+++ src/app/Products/[slug]/page.js
@@ -1,24 +1,23 @@
 
+"use client"
+
+import {useRecoilValue} from "recoil";
+import {ComicsState} from "@/app/_libs/States/ComicsState";
 
 
-export default function Products (){
+export default function Products ({params}){
 
 
-
-
+    const slugId= params.slug;
+    const comicsValue=useRecoilValue(ComicsState)
+    console.log(slugId)
     // if( !data ){
     //     return <h1>Ova stranice ne postoji</h1>
     // }
 
     return<>
 
-        <div>
-            <h3>test</h3>
-            <p>test2</p>
-            <p>test3</p>
-            <b>test2</b>
-            <p>test</p>
-        </div>
+        <p>Hello world</p>
 
     </>
 
