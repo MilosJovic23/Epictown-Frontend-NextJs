d7210c40cc3f1d3cee6bbd28f2854d14f85a71d8
MilosJovic23 <milos.mj992@gmail.com>
2024-09-17 13:32:11 +0200

    * small changes

--- src/app/_components/serachByTitle.jsx
+++ src/app/_components/serachByTitle.jsx
@@ -5,66 +5,67 @@
 import {useRecoilValue} from "recoil";
 import {ComicsState} from "@/app/_libs/States/ComicsState";
 import {useEffect, useState} from "react";
-
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = ()=>{
 
-    const [searchTerm,setSearchTerm]=useState([]);
+    const [searchTerm,setSearchTerm]=useState('');
+    const [searchResults,setSearchResults]=useState([]);
 
     const comics= useRecoilValue(ComicsState);
 
-    let searchByTitleId= [];
+
 
 
     const search = ()=>{
+
 
         comics.forEach( comic =>{
 
            if( comic.title.toLowerCase().includes(searchTerm.toLowerCase()) || comic.author.toLowerCase().includes(searchTerm.toLowerCase()) ){
 
-               searchByTitleId.push(comic);
+               setSearchResults([...items, `${items.length + 1}`]);
+               console.log(searchResults);
            }
         }
         )
     }
-    useEffect(() => {
-        searchByTitleId=[];
-    }, [search]);
+    // useEffect(() => {
+    //     searchByTitleId=[];
+    // }, [search]);
 
 
 
     return<>
 
         <div>
-            <form >
-                <input type="text" placeholder="search comics" onChange={e=>setSearchTerm(e.currentTarget.value)}/>
-                <button type="button" onClick={search}>search</button>
+            <form className="form-field">
+                <input  placeholder="Search comics" o onChange={e => setSearchTerm(e.currentTarget.value)}/>
+                <span className="icon"><AiOutlineSearch /></span>
+                <button type="button" onClick={search} >search</button>
             </form>
-            {
+            {/*<div>*/}
 
-                !searchByTitleId?( searchByTitleId.map((comic,index)=> {
+            {/*    {*/}
+            {/*        searchResults.map((comic,index)=> {*/}
 
-                        return <div  key={index}>
-                            <a href={`/Products/${comic.id}`} target="_blank">
-                                <div >
-                                    <img src={comic.imgURL}/>
-                                </div>
-                                <div >
-                                    <h3>{comic.title}</h3>
-                                    <p>author:{comic.author}</p>
-                                </div>
-                            </a>
-                        </div>
+            {/*            return <div  key={index}>*/}
+            {/*                <a href={`/Products/${comic.id}`} target="_blank">*/}
+            {/*                    <div >*/}
+            {/*                        <img src={comic.imgURL}/>*/}
+            {/*                    </div>*/}
+            {/*                    <div >*/}
+            {/*                        <h3>{comic.title}</h3>*/}
+            {/*                    </div>*/}
+            {/*                </a>*/}
+            {/*            </div>*/}
 
-                    }
-                )
-                ):(
-                ''
-                )
+            {/*        })*/}
 
 
+            {/*    }*/}
+            {/*</div>*/}
 
-            }
         </div>
 
     </>
