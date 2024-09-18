d6109dfa24e03629c289fab7a05934e95149a2b4
MilosJovic23 <milos.mj992@gmail.com>
2024-09-18 13:50:54 +0200

    * search component setup

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,20 @@
 import ComicList from "@/app/_components/ComicList";
 import Search from "@/app/_components/serachByTitle";
 
-
+import "./MainPageStyle.css";
 
 export default function Home() {
   return (
-    <div className="comicListWrapper">
-      {/*<ComicList/>*/}
-       <Search/>
+    <div >
+        <div className="comicSearch">
+            <Search/>
+        </div>
+
+        <div className="comicListWrapper">
+            <ComicList/>
+        </div>
+
+
     </div>
   );
 }
