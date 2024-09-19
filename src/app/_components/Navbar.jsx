78d0b5aded70ff971eb7bd30a6710101f4bf5b3a
MilosJovic23 <milos.mj992@gmail.com>
2024-09-19 17:13:43 +0200

    * Navbar component setup

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -1,25 +1,28 @@
 import Search from "@/app/_components/serachByTitle";
+import Image from 'next/image'
+import logo from '../epictown.svg'
 
-
+import 'bootstrap/dist/css/bootstrap.css'
 const Navbar =()=>{
     return<>
 
 
     <div className="navBar">
-        <div className="container">
-            <div>
-                <img src="" alt=""/>
-                <div>
-                    <ul>
-                        <a>about</a>
-                        <a>blog</a>
-                        <a>contact</a>
+        <div className="container d-flex  justify-content-between">
+            <div className="d-flex w-50 gap-5">
+                <Image className="logo-img" src={logo} alt="Picture of the author" width="20px" height="20px"/>
+                <div className="navigation d-flex align-items-center">
+                    <ul className="d-flex gap-4">
+                        <a href={`/About`}>about</a>
+                        <a href={`/Blog`}>blog</a>
+                        <a href={`/Contact`}>contact</a>
                     </ul>
                 </div>
             </div>
-            <div>
+            <div className="d-flex gap-3">
                 <Search/>
-                <a>Login</a>
+                <div className="vr"></div>
+                <a className="align-self-center" href={`/users/login`}>login</a>
             </div>
         </div>
     </div>
