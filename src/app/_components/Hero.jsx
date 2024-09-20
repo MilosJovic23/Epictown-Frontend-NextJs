41303b30266fa945712dfbfc6c489d2cb7cd8e5b
MilosJovic23 <milos.mj992@gmail.com>
2024-09-20 13:55:51 +0200

    * Home page setup

---
+++ src/app/_components/Hero.jsx
@@ -0,0 +1,31 @@
+
+
+
+import "../Hero.css"
+import 'bootstrap/dist/css/bootstrap.css'
+const Hero = () =>{
+
+    return<>
+
+
+        <section className="heroSection">
+            <div className="MainContainer d-flex justify-content-between">
+                <div className="hero-col">
+                    <h1>Graphic novels comic books and more..</h1>
+                    <a href="#">explore</a>
+                </div>
+                <div className="hero-col">
+                    <h1>Graphic novels comic books and more..</h1>
+                    <a href="#">explore</a>
+                </div>
+            </div>
+
+        </section>
+
+    </>
+
+
+}
+
+
+export default Hero;
