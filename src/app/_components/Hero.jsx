010c53dce08ed9ff4b6edddc32c9ba09445f16dc
MilosJovic23 <milos.mj992@gmail.com>
2024-09-24 13:32:16 +0200

    * small changes

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -1,9 +1,37 @@
 
-
+"use client"
 
 import "../Hero.css"
 import 'bootstrap/dist/css/bootstrap.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import {useRecoilState} from "recoil";
+import {ComicsState} from "@/app/_libs/States/ComicsState";
 const Hero = () =>{
+
+    const [comics, setComics] = useRecoilState(ComicsState);
+
+    const responsive = {
+        superLargeDesktop: {
+
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 2
+
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
 
     return<>
 
@@ -11,11 +39,25 @@ const Hero = () =>{
             <div className="MainContainer d-flex justify-content-between">
                 <div className="hero-col">
                     <h1>Graphic novels comic books and more..</h1>
-                    <a href="#">explore</a>
+                    <a id="exploreBtn" href="#">explore</a>
                 </div>
                 <div className="hero-col">
-                    <h1>Graphic novels comic books and more..</h1>
-                    <a href="#">explore</a>
+                    <Carousel  responsive={responsive}>
+                        {
+                            comics.map((comic,index)=>{
+                                return <div className="comicCarousel" key={index}>
+                                    <a href={`/Products/${comic.id}`} target="_blank">
+                                        <div className="comicCarouselImg">
+                                            <img src={comic.imgURL}/>
+                                        </div>
+
+
+                                    </a>
+                                </div>
+
+
+                            })}
+                    </Carousel>
                 </div>
             </div>
 
