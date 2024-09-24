70d4842657bf41b5d9425b1c99cf09ad2541ccfb
MilosJovic23 <milos.mj992@gmail.com>
2024-09-24 13:18:34 +0200

    * small changes

--- src/app/_components/ComicList.jsx
+++ src/app/_components/ComicList.jsx
@@ -21,24 +21,33 @@ const ComicList = ()=>{
 
 
     return<>
+        <div className="MainContainer py-5">
 
-        {
-            comics.map((comic)=>{
-            return <div className="singleComicWrapper" key={comic.id}>
-                <a href={`/Products/${comic.id}`} target="_blank">
-                    <div className="singleComicImg">
-                        <img src={comic.imgURL}/>
+        <div className="d-flex justify-content-between py-3">
+            <a>Best sellers</a>
+            <a>view all</a>
+        </div>
+        <div className="d-flex flex-wrap gap-1 align-self-center">
+            {
+                comics.map((comic)=>{
+                    return <div className="singleComicWrapper" key={comic.id}>
+                        <a href={`/Products/${comic.id}`} target="_blank">
+                            <div className="singleComicImg">
+                                <img src={comic.imgURL}/>
+                            </div>
+                            <div className="comicTitle text-dark">
+                                <h5>{comic.title}</h5>
+                                <p>author:{comic.author}</p>
+                            </div>
+
+                        </a>
                     </div>
-                    <div className="comicTitle text-dark">
-                        <h5>{comic.title}</h5>
-                        <p>author:{comic.author}</p>
-                    </div>
-
-                </a>
-            </div>
 
 
-        })}
+                })}
+        </div>
+
+        </div>
     </>
 }
 
