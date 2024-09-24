78b999789d153aa293c763a892e737d2d1fc9d82
MilosJovic23 <milos.mj992@gmail.com>
2024-09-24 14:35:00 +0200

    * small changes

--- src/app/_components/ComicList.jsx
+++ src/app/_components/ComicList.jsx
@@ -16,21 +16,22 @@ const ComicList = ()=>{
         fetchComics(setComics);
     }, []);
 
-
-    console.log(comics)
+    const sortedProducts = [...comics].sort((a, b) => b.rating - a.rating).slice(0,7)
+    console.log(sortedProducts)
 
 
     return<>
-        <div className="MainContainer py-5">
+        <div className="MainContainer py-4">
 
         <div className="comici-list-text d-flex justify-content-between py-3">
-            <a>Best sellers</a>
+            <a>Best rated</a>
             <a href='/products'>view all</a>
         </div>
-        <div className="d-flex flex-wrap gap-1 align-self-center">
+        <div className="d-flex flex-wrap gap-0 align-self-center justify-content-between">
             {
-                comics.map((comic)=>{
-                    return <div className="singleComicWrapper" key={comic.id}>
+                sortedProducts.map((comic,index)=>{
+
+                    return <div className="singleComicWrapper" key={index}>
                         <a href={`/Products/${comic.id}`} target="_blank">
                             <div className="singleComicImg ">
                                 <img src={comic.imgURL}/>
@@ -38,10 +39,13 @@ const ComicList = ()=>{
                             <div className="comicTitle text-dark">
                                 <h5>{comic.title}</h5>
                                 <p>author:{comic.author}</p>
+
                             </div>
 
                         </a>
                     </div>
+
+
 
 
                 })}
