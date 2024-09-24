6d40355921e10fddc6d55453ee4c0ddcda109c0e
MilosJovic23 <milos.mj992@gmail.com>
2024-09-24 14:35:48 +0200

    * small changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,16 @@
-import localFont from "next/font/local";
+
 import "./globals.css";
 
-
+import { DM_Sans } from "@next/font/google"
 
 import RecoilContextProvider from "@/app/_libs/RecoilContextProvider";
 
+    const dmSans_init = DM_Sans ({
+        subsets: ['latin'],
+        weight: ['100','300','400','500'],
+        variable: '--font-DMsans'
+});
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -25,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={dmSans_init.variable} >
       <RecoilContextProvider>
           {children}
       </RecoilContextProvider>
