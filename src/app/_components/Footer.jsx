7ec81991a701f3d508786a005cec0a9100e45bc0
MilosJovic23 <milos.mj992@gmail.com>
2024-09-27 16:44:41 +0200

    * small changes

--- src/app/_components/Footer.jsx
+++ src/app/_components/Footer.jsx
@@ -3,15 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../footer.css'
 import Image from "next/image";
 import logo from "@/app/_images/epictown.svg";
+import { BsFacebook, BsInstagram, BsTwitterX } from "react-icons/bs";
 const Footer = () =>{
 
 
     return <>
       <footer className="footerContainer">
 
-          <div className="MainContainer d-flex py-5 flex-wrap">
-              <div className="col">
-                  <ul className="mx-5 text-end">
+          <div className="MainContainer d-flex py-5 flex-wrap justify-content-between">
+              <div className="col3">
+                  <ul>
                       <li><a href='#'>about</a></li>
                       <li><a href='#'>blog</a></li>
                       <li><a href='#'>contact</a></li>
@@ -19,8 +20,16 @@ const Footer = () =>{
                   </ul>
               </div>
               <div className="vr"></div>
-              <div className="col">
-                  <ul className="mx-5 text-end">
+              <div className="col3">
+                  <ul>
+                      <li><a href='#'>about</a></li>
+                      <li><a href='#'>blog</a></li>
+                      <li><a href='#'>contact</a></li>
+                  </ul>
+              </div>
+              <div className="vr"></div>
+              <div className="col3">
+                  <ul >
                       <li><a href='#'>about</a></li>
                       <li><a href='#'>blog</a></li>
                       <li><a href='#'>contact</a></li>
@@ -28,21 +37,15 @@ const Footer = () =>{
                   </ul>
               </div>
               <div className="vr"></div>
-              <div className="col">
-                  <ul className="mx-5 text-end ">
-                      <li><a href='#'>about</a></li>
-                      <li><a href='#'>blog</a></li>
-                      <li><a href='#'>contact</a></li>
-                      <li><a href='#'>login</a></li>
-                  </ul>
-              </div>
-              <div className="vr"></div>
-              <div className="col">
-                  <div className="mx-5 text-end">
-                      <a href="/"><Image className="logo-img" src={logo} alt="Picture of the author" width="20px"
-                                         height="20px"/></a>
-                      <p>Hello world</p>
-                      <p>Hello world</p>
+              <div className="col3 justify-content-sm-center">
+                  <div className="mx-5 d-flex flex-column gap-2">
+                      <a href="/"><Image className="logo-img" src={logo} alt="Picture of the author" width="20px" height="20px"/></a>
+                      <p className="mt-5 mb-0">FOLLOW US</p>
+                      <div className="d-flex  gap-2">
+                          <span><BsInstagram/></span>
+                          <span><BsFacebook/></span>
+                          <span><BsTwitterX/></span>
+                      </div>
                   </div>
 
 
