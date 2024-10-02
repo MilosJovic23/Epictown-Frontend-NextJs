ab44c5fb947a894a25f5adcc08d822859fb5cd1c
MilosJovic23 <milos.mj992@gmail.com>
2024-10-02 12:45:22 +0200

    * adding Blog component to the project

--- src/app/_components/Favorites.jsx
+++ src/app/_components/Favorites.jsx
@@ -4,11 +4,13 @@
 import { HiOutlineStar,HiStar } from "react-icons/hi2";
 import { useRecoilState } from "recoil";
 import { FavoriteComics } from "@/app/_libs/States/Favorites";
+import {UserState} from "@/app/_libs/States/UserState";
 
 
 const Favorites = ({comicId}) =>{
 
     const [favorites, setFavorites] = useRecoilState(FavoriteComics);
+    const [userState,setUserState]=useRecoilState(UserState);
 
     const addToFavorites = () =>{
 
@@ -27,18 +29,18 @@ const Favorites = ({comicId}) =>{
 
 
     return <>
-        <div onClick= { addToFavorites } className="like-button" style={{ cursor: 'pointer' }}>
         {
-            !isFavorite ?  (
-                <HiOutlineStar style={{ fontSize: '1.7rem', color: 'black' }} />
-            ):(
-                <HiStar style={{ fontSize: '1.7rem', color: 'gold' }} />
-            )
+            userState.isLoggedIn && (<div onClick={addToFavorites} className="like-button" style={{cursor: 'pointer'}}>
+                {
+                    !isFavorite ? (
+                        <HiOutlineStar style={{fontSize: '1.7rem', color: 'black'}}/>
+                    ) : (
+                        <HiStar style={{fontSize: '1.7rem', color: 'gold'}}/>
+                    )
+                }
+            </div>)
+
         }
-        </div>
-
-
-
 
 
     </>
