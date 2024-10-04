8266484a0d58df211715f2ba180c0692916d6994
MilosJovic23 <milos.mj992@gmail.com>
2024-10-04 18:39:31 +0200

    * setting up site for responsive in bootstrap

--- src/app/_functions/fetchBlogPosts.js
+++ src/app/_functions/fetchBlogPosts.js
@@ -3,19 +3,19 @@ import {collection, getDocs} from "firebase/firestore";
 import {db} from "@/app/firebase";
 
 
-const fetchBlogPosts= async(setBlogPosts)=>{
+const fetchBlogPosts = async( setBlogPosts )=>{
 
     try{
-        const blogCollection = collection(db, 'blog');
+        const blogCollection = collection( db, 'blog' );
 
-        const comicSnapshot = await getDocs(blogCollection);
+        const comicSnapshot = await getDocs( blogCollection );
 
         const blogList = comicSnapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data(),
         }));
         setBlogPosts(blogList);
-    }catch (error) {
+    }catch ( error ) {
         console.error('Error fetching blog posts: ', error);
     }
 };
