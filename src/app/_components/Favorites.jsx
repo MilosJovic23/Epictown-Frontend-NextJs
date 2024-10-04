d850dec4ea4cc8e23f98e76a98785a4826f4e864
MilosJovic23 <milos.mj992@gmail.com>
2024-10-04 18:39:13 +0200

    * setting up site for responsive in bootstrap

--- src/app/_components/Favorites.jsx
+++ src/app/_components/Favorites.jsx
@@ -7,25 +7,25 @@ import { FavoriteComics } from "@/app/_libs/States/Favorites";
 import {UserState} from "@/app/_libs/States/UserState";
 
 
-const Favorites = ({comicId}) =>{
+const Favorites = ( { comicId } ) =>{
 
-    const [favorites, setFavorites] = useRecoilState(FavoriteComics);
-    const [userState,setUserState]=useRecoilState(UserState);
+    const [favorites, setFavorites] = useRecoilState( FavoriteComics );
+    const [userState,setUserState]=useRecoilState( UserState );
 
     const addToFavorites = () =>{
 
-        setFavorites((prevFavorites) => {
+        setFavorites(( prevFavorites ) => {
 
-            const currentFavorites = Array.isArray(prevFavorites) ? prevFavorites : [];
+            const currentFavorites = Array.isArray( prevFavorites ) ? prevFavorites : [];
 
-            return currentFavorites.includes(comicId)
-                ? currentFavorites.filter((id) => id !== comicId)
+            return currentFavorites.includes( comicId )
+                ? currentFavorites.filter( ( id ) => id !== comicId )
                 : [...currentFavorites, comicId]
         });
 
     }
 
-        const isFavorite = favorites.includes(comicId);
+        const isFavorite = favorites.includes( comicId );
 
 
     return <>
@@ -33,9 +33,9 @@ const Favorites = ({comicId}) =>{
             userState.isLoggedIn && (<div onClick={ addToFavorites } className="like-button" style={{ cursor: 'pointer' }}>
                 {
                     !isFavorite ? (
-                        <HiOutlineStar style={{fontSize: '1.7rem', color: 'black'}}/>
+                        <HiOutlineStar style={{ fontSize: '1.7rem', color: 'black' }}/>
                     ) : (
-                        <HiStar style={{fontSize: '1.7rem', color: 'gold'}}/>
+                        <HiStar style={{ fontSize: '1.7rem', color: 'gold' }}/>
                     )
                 }
             </div>)
