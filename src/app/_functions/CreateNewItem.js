1212849515d401690f484b301b7d1091e54ae265
MilosJovic23 <milos.mj992@gmail.com>
2024-10-08 13:25:59 +0200

    * setting up CRUD from firestore

--- src/app/_functions/CreateNewItem.js
+++ src/app/_functions/CreateNewItem.js
@@ -1,7 +1,7 @@
 
 
 
-import { collection,setDoc,doc } from "firebase/firestore"
+import {collection, setDoc, doc, addDoc} from "firebase/firestore"
 import {db} from "@/app/firebase";
 
 
@@ -10,7 +10,7 @@ const CreateNewItem = async (data) => {
 
     try{
         const comicCollection = collection( db,"comics");
-        await setDoc(doc(comicCollection),data);
+        await addDoc(doc(comicCollection),data);
     }
     catch(error){
         console.log("Error adding document: ", error)
