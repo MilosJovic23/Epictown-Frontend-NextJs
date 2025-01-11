56b64ce4b54c75989f49d0f60431f8ab34b37776
MilosJovic23 <milos.mj992@gmail.com>
2025-01-11 10:59:22 +0100

    * setting up search api endpoint

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,20 +4,29 @@
 export default async function handler (req,res) {
 
     if( req.method !== 'GET' ){
-        return throw new Error( "Method not implemented,can only handle GET method" );
+        return res.status(405).json( {message:"Method not implemented,can only handle GET method"} );
     }
+
     try {
+
+        const query = req.query;
+
         const response = await fetch(process.env.API_ENDPOINT,{
             method: "POST",
             header:{
                 "Content-Type": "application/json",
             },
-            body : JSON.stringify({search:"batman"}),
+            body : JSON.stringify({ search: query }),
 
         })
         const data = await response.json();
-        return res.status(200).json({data})
+        console.log(data);
+
+        res.status(200).json({data});
+
+
     }
+
     catch(error){
         console.error(error);
     }
