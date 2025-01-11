878c3f05c4476266323a8be525e4c9451b9444ec
MilosJovic23 <milos.mj992@gmail.com>
2025-01-11 18:59:51 +0100

    * setting up search api endpoint

--- pages/api/search.js
+++ pages/api/search.js
@@ -11,24 +11,24 @@ export default async function handler (req,res) {
 
         const query = req.query;
 
-        const response = await fetch(process.env.API_ENDPOINT,{
+        const response = await fetch(process.env.SEARCH_API_URL,{
             method: "POST",
             header:{
                 "Content-Type": "application/json",
             },
-            body : JSON.stringify({ search: query }),
+            body : JSON.stringify({ "search": req.query.search }),
 
         })
-        const data = await response.json();
-        console.log(data);
 
+        const data = await response.json();
         res.status(200).json({data});
 
 
     }
 
     catch(error){
-        console.error(error);
+        console.error("Error fetching data from PHP API:", error);
+        res.status(500).json({ message: "Failed to fetch data from PHP API", error: error.message });
     }
 
 }
