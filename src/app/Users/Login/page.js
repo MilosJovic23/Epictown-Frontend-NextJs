789a0a80b6ccc6008b87ed45247d5ced77856e6c
MilosJovic23 <milos.mj992@gmail.com>
2025-01-27 09:48:42 +0100

    * adding register form to login page

--- src/app/Users/Login/page.js
+++ src/app/Users/Login/page.js
@@ -14,13 +14,22 @@ import {useState} from "react";
 
 export default function Login (){
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const {
+        register:registerForm1,
+        handleSubmit:handleSubmit1,
+        formState: { errors1 }
+    } = useForm();
+    const {
+        register:registerForm2,
+        handleSubmit:handleSubmit2,
+        formState: { errors2 }
+    } = useForm();
     const router = useRouter();
     const [userState,setUserState]= useRecoilState(UserState);
-    const [message, setMessage] = useState("");
+    const [messageForm1, setMessageForm1] = useState("");
+    const [messageForm2, setMessageForm2] = useState("");
 
-
-    const onSubmit = async (data) => {
+    const onSubmit1 = async (data) => {
 
         try{
             const response = await fetch(process.env.NEXT_PUBLIC_LOGIN_URL, {
@@ -43,50 +52,81 @@ export default function Login (){
                 router.push('/');
             } else {
                 const errorData = await response.json();
-                setMessage(`Login failed: ${errorData.message}`);
+                setMessageForm1(`Login failed: ${errorData.message}`);
             }
         }
         catch(error){
             console.error("Error during login:", error);
-            setMessage("An error occurred. Please try again.");
+            setMessageForm1("An error occurred. Please try again.");
         }
     };
 
+    const onSubmit2 = async (data) => {
+
+    }
 
     return <>
 
         <Header/>
 
-        <div className="MainContainer d-flex align-items-center" >
-            <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <div className="MainContainer d-flex align-items-center">
+            <form onSubmit={handleSubmit1(onSubmit1)} className="form">
 
 
-                <div  className="formGroup">
+                <div className="formGroup">
                     <input
                         type="email"
                         placeholder="Email"
-                        {...register('email', {required: 'Email is required'})}
+                        {...registerForm1('email', {required: 'Email is required'})}
                         className="input"
                     />
-                    {errors.email && <p>{errors.email.message}</p>}
-                    { message && (<p>{message}</p>)}
+                    {errors1.email && <p>{errors1.email.message}</p>}
+                    {messageForm1 && (<p>{messageForm1}</p>)}
                 </div>
 
                 <div className="formGroup">
                     <input
                         type="password"
                         placeholder="Password"
-                        {...register('password', {required: 'Password is required'})}
+                        {...registerForm1('password', {required: 'Password is required'})}
                         className="input"
                     />
-                    {errors.password && <p>{errors.password.message}</p>}
+                    {errors1.password && <p>{errors1.password.message}</p>}
                 </div>
 
                 <button type="submit" className="button">Login</button>
+
+            </form>
+
+            <form onSubmit={handleSubmit2(onSubmit2)} className="form">
+                <p>register here</p>
+
+                <div className="formGroup">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        {...registerForm2('email', {required: 'Email is required'})}
+                        className="input"
+                    />
+                    {errors2.email && <p>{errors2.email.message}</p>}
+                    {messageForm2 && (<p>{messageForm2}</p>)}
+                </div>
+
+                <div className="formGroup">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        {...registerForm2('password', {required: 'Password is required'})}
+                        className="input"
+                    />
+                    {errors2.password && <p>{errors2.password.message}</p>}
+                </div>
+
+                <button type="submit" className="button">Login</button>
+
             </form>
         </div>
         <Footer/>
     </>
-
 
 };
