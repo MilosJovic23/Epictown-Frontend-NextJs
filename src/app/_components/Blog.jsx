ef8ca7b539ac39e7fa74dba18323ca7470d799f5
MilosJovic23 <milos.mj992@gmail.com>
2025-02-03 18:16:21 +0100

    * few changes

--- src/app/_components/Blog.jsx
+++ src/app/_components/Blog.jsx
@@ -10,10 +10,30 @@ const Blog = ()=>{
     return <>
 
         <section className="blogSection">
-
             <main className="MainContainer display-grid">
-                {
+                    {
+                        blogPosts.map( (blog,index) => {
+                            return <>
+                                <article key={ index } className={`blogItem item blog-item-${ blog.id }`}>
+                                    <a href={`/Blog/${ blog.id }`}>
+                                        <div>
+                                            <div>
+                                                <h2>{ blog.title }</h2>
+                                            </div>
+                                            <div>
+                                                <p>{ blog.date }</p>
+                                            </div>
+                                        </div>
+                                        <i>{ blog.description }</i>
+                                        {
+                                            blog.img && <img src={ blog.img } alt={ blog.title }/>
+                                        }
 
+                                    </a>
+
+                                </article>
+                            </>
+                        })
                 }
 
             </main>
