c008ffde5d95b120e7946f75523b15e38b83b1a3
MilosJovic23 <milos.mj992@gmail.com>
2025-02-07 09:32:23 +0100

    * cleared unused lines of code

--- pages/api/search.js
+++ pages/api/search.js
@@ -22,7 +22,6 @@ export default async function handler (req,res) {
         res.status(200).json({data});
 
     }
-
     catch(error){
         console.error("Error fetching data from PHP API:", error);
         res.status(500).json({ message: "Failed to fetch data from PHP API", error: error.message });
