071594857d9525731e2ab8bbf117945cbfc2f2e7
MilosJovic23 <milos.mj992@gmail.com>
2025-10-06 17:12:56 +0200

    removed form

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -17,22 +17,6 @@ export default function  Contact() {
             <div className="row">
                 <div className="col-md-6">
                     <h2>Get in Touch</h2>
-                    <form>
-                        <div className="mb-3">
-                            <label htmlFor="name" className="form-label">Name</label>
-                            <input type="text" className="form-control" id="name" placeholder="Your Name" required/>
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="email" placeholder="Your Email" required/>
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="message" className="form-label">Message</label>
-                            <textarea className="form-control" id="message" rows="4" placeholder="Your Message"
-                                      required></textarea>
-                        </div>
-                        <button type="submit" className="btn btn-primary">Send Message</button>
-                    </form>
                 </div>
                 {/*<div className="col-md-6">*/}
                 {/*    <h2>Contact Details</h2>*/}
